ae6bb6ae1fc3a239f814511394adbfb7c19e2cd8
Almightyjuju <alexanderpizzati11@gmail.com>
2023-10-06 08:29:48 -0500

    git create a new folder

---
+++ block07/bloc7.js
@@ -0,0 +1,23 @@
+/* - message for user 
+   - const is being used as the variable because the message wont change */
+
+
+const message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: "
+
+
+/* -  each problem is assigned to a specific variable  
+-  this will make it easier for users to read each number */
+   
+
+let numOne = 5 + 5 
+
+let numTwo = 4 * 10 
+
+let numThree = 90 - 51
+
+console.log(numOne + " - " + numTwo + " - " + numThree) 
+
+alert(message + numOne + " - " + numTwo + " - " + numThree)  
+
+
+// -  make sure to leave spaces between em dashes and double qout
