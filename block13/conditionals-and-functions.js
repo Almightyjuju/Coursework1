23f7ce3d2d9c3b3d9e3f8adb209fab2ead2cda93
Almightyjuju <alexanderpizzati11@gmail.com>
2023-10-16 11:00:10 -0500

    conditionals and functions examples

---
+++ block13/conditionals-and-functions.js
@@ -0,0 +1,16 @@
+const itsRaining = false;
+let itsSunny = true;
+
+console.log(itsRaining);
+console.log(itsSunny);
+
+if (17) {
+  console.log("This is truthy");
+} else {
+  console.log("this is falsey");
+}
+if (itsRaining) {
+  console.log("Dont forget youyr umbrella!");
+} else {
+  console.log("Enjoy the sunshine");
+}
